81a38a00463c3802faf4eaf4df768924e6d82d68
anindameister <anindameister0@gmail.com>
2020-04-17 14:31:18 +0200

    iterating and reading the content

--- txt2xclCSV.py
+++ txt2xclCSV.py
@@ -2,4 +2,7 @@ import csv
 
 with open('Book1.csv', 'r') as csv_file: #same directory as the file that am currently writing
     csv_reader = csv.reader(csv_file) #reader method
-    print(csv_reader) #<_csv.reader object at 0x000001F4CEF68F28> , just an object in memory
+    #print(csv_reader) #<_csv.reader object at 0x000001F4CEF68F28> , just an object in memory
+    for line in csv_reader:
+        print(line) #['Image name', 'Object number', 'X1', 'Y1', 'X2', 'Y2', 'object name']
+                    #['download (1).jpg.txt', '2', '26', '22', '277', '144', 'car']
