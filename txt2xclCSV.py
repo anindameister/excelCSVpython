8b17644522efa3d2a44c68213b98ed2799e83382
anindameister <anindameister0@gmail.com>
2020-04-17 23:18:18 +0200

    using DictReader

--- txt2xclCSV.py
+++ txt2xclCSV.py
@@ -1,14 +1,7 @@
 import csv
 
 with open('Book1.csv', 'r') as csv_file: #same directory as the file that am currently writing
-    csv_reader = csv.reader(csv_file) #reader method
-    
-    
+    csv_reader = csv.DictReader(csv_file) #reader method
 
-    with open('new_Book1.csv', 'w') as new_file:
-        csv_writer =csv.writer(new_file, delimiter='-')
-
-
-        for line in csv_reader:
-            csv_writer.writerow(line)
-        
+    for line in csv_reader:
+        print(line['Object number'])
