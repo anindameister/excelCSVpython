3e6f220abb68b26dad152c8fec5b3b523d3be84a
anindameister <anindameister0@gmail.com>
2020-04-17 14:15:18 +0200

    comments, just an object in memory, a step back

--- txt2xclCSV.py
+++ txt2xclCSV.py
@@ -1,7 +1,5 @@
 import csv
 
-with open('Book1.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-
-    for line in csv_reader:
-        print(line)
+with open('Book1.csv', 'r') as csv_file: #same directory as the file that am currently writing
+    csv_reader = csv.reader(csv_file) #reader method
+    print(csv_reader) #<_csv.reader object at 0x000001F4CEF68F28> , just an object in memory
