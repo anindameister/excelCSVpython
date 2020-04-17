973c65238deb46a947aaca367c50dd1878455a3e
anindameister <anindameister0@gmail.com>
2020-04-17 16:35:18 +0200

    creating new csv file, instead of default ',' used '-'

--- txt2xclCSV.py
+++ txt2xclCSV.py
@@ -2,10 +2,13 @@ import csv
 
 with open('Book1.csv', 'r') as csv_file: #same directory as the file that am currently writing
     csv_reader = csv.reader(csv_file) #reader method
-    #print(csv_reader) #<_csv.reader object at 0x000001F4CEF68F28> , just an object in memory
-    next(csv_reader) #just the values under x1, without the mention of x1
-    for line in csv_reader:
-        #print(line) #['Image name', 'Object number', 'X1', 'Y1', 'X2', 'Y2', 'object name'], field names
-                    #['download (1).jpg.txt', '2', '26', '22', '277', '144', 'car'], list of all the values
-        print(line[2]) #x1 is 3rd hence 2, values under x1
+    
+    
+
+    with open('new_Book1.csv', 'w') as new_file:
+        csv_writer =csv.writer(new_file, delimiter='-')
+
+
+        for line in csv_reader:
+            csv_writer.writerow(line)
         
